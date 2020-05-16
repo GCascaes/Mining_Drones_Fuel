447aee17ae773c935529532ad91429b537596aab
GCascaes <gabriel.m.cascaes@gmail.com>
2020-05-16 14:27:45 -0300

    Fixed some dumb syntax errors
    Fixed some dumb syntax errors

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,5 +1,5 @@
 for name,recipe in pairs(data.raw.recipe) do
     if recipe.category == "mining-depot" then
-        recipe.ingredients[2] = {type = "fluid", name = settings.startup["mining-drones-fuel"], amount = settings.startup["mining-drones-fuel-comsumption"]}
+        recipe.ingredients[2] = {type = "fluid", name = settings.startup["mining-drones-fuel"].value, amount = settings.startup["mining-drones-fuel-comsumption"].value}
     end
 end
--- settings.lua
+++ settings.lua
@@ -1,16 +1,19 @@
-data:extend({
+local settings = 
+{
     {
-        name = "mining-drones-fuel",
         type = "string-setting",
-        setting-type = "startup",
+        name = "mining-drones-fuel",
+        setting_type = "startup",
         localised_name = "Fuel used by the mining drones",
         default_value = (mods["KS_Power"] and "diesel-fuel") or "petroleum-gas"
     },
     {
         name = "mining-drones-fuel-comsumption",
         type = "int-setting",
-        setting-type = "startup",
+        setting_type = "startup",
         localised_name = "Amount of fuel used by the mining drones",
         default_value = 1
     }
-})
+}
+
+data:extend(settings)
